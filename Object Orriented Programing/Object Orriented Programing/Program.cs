361c67ec3c78ac540b586b0b1ca5cefbea7a528f
Xavier Martin <golf4xavier@gmail.com>
2022-05-24 07:58:07 -0700

    adding vs code

---
+++ Object Orriented Programing/Object Orriented Programing/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//OOP
+//Object Orriented Programing
+//4 pillars of OOP - Abstraction, Polymorphism, inheritance, encapsulation
+//outline classes and objects at a macro level. Also outlines the features and relationships of clases and objects.
+//objects: tangale items we can use/create. Also instances of classes.
+//instances: objects are specific examples of classes.
+//Properties: objects that are refrenced based on specific characteristics
+//Method: functions used and manipulate the characteristics of the specific object.
+//difference between methods and properties: both use dot operator, but properties use a parentheses after the method name. Methods do not use parentheses.
+// property: ex. names.Count;
+// method: ex. names.MyMethod();
+
+
+//Classes
+//object types.
+//Classes outline what properties and methods a particular object will have at its fingertips.
+
+
+//Classes vs. objects
+
